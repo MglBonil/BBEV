f1d99ad3b86056c993b56e4e519baab8d9f50df7
Keyz-web <kauasf0504@gmail.com>
2026-07-07 10:40:12 -0300

    Miguel ve se vc acha o que ta dando errado

--- JS/Scripts/cadProf.js
+++ JS/Scripts/cadProf.js
@@ -1,17 +1,57 @@
 import { hashSenha } from "../Utils/hashSenha.js";
 
 async function cadProf(event) {
-
-    
     event.preventDefault();
 
-    const form = document.getElementById("formProf");
+    console.log("Função executada");
 
+    const rmProf = document.getElementById("rmProf").value.trim();
+    const nomeProf = document.getElementById("nomeProf").value.trim();
+    const emailProf = document.getElementById("emailProf").value.trim();
+    const senhaProf = document.getElementById("senhaProf").value.trim();
+
+    //Validar se todos os campos estão preenchidos
+    if (!rmProf || !nomeProf || !emailProf || !senhaProf) {
+        alert("Todos os campos devem estar preenchidos!");
+        return;
+    }
+
+    //Validar RM: Garante que são APENAS números E que tem exatamente 5 dígitos
+    if (!/^\d{5}$/.test(rmProf)) {
+        alert("RM inválido! Deve conter exatamente 5 números.");
+        return;
+    }
+
+    //Validar limite do Nome
+    if (nomeProf.length > 30) {
+        alert("O nome deve ter no máximo 30 caracteres.");
+        return;
+    }
+
+    //Validar formato do Email
+    if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailProf)) {
+        alert("Email inválido!");
+        return;
+    }
+
+    //Validar limite do Email
+    if (emailProf.length > 30) {
+        alert("O Email deve ter no máximo 30 caracteres.");
+        return;
+    }
+
+    // Validar Senha Entre 8 e 32 caracteres, aceitando qualquer caractere, mas exigindo pelo menos um especial ($*&@#)
+    if (!/^(?=.*[$*&@#]).{8,32}$/.test(senhaProf)) {
+        alert("A senha deve ter entre 8 e 32 caracteres e conter pelo menos um caractere especial ($*&@#).");
+        return;
+    }
+
+    
     const novoProfessor = {
-        rmProf: Number(document.getElementById("rmProf").value),
-        nomeProf: document.getElementById("nomeProf").value.trim(),
-        emailProf: document.getElementById("emailProf").value.trim(),
-        senhaProf: await hashSenha(document.getElementById("senhaProf").value),
+        rmProf: Number(rmProf),
+        nomeProf: nomeProf,
+        emailProf: emailProf,
+        senhaProf: await hashSenha(senhaProf),
         tipo: "Professor",
         statusProf: true
     };
@@ -34,13 +74,10 @@ async function cadProf(event) {
         console.log(result);
         alert("Professor cadastrado com sucesso!");
         window.location.href = "listProf.html";
+
     } catch (error) {
         console.error("Erro ao cadastrar:", error);
-
-        if (error instanceof TypeError) {
-            return;
-        }
-
+        if (error instanceof TypeError) return;
         alert(error.message || "Erro ao cadastrar professor.");
     }
 }
