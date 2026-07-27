3c298d3b474797b90020b93d3ba4d51f2d29cf5e
Miguel Bonilha <126339796+MglBonil@users.noreply.github.com>
2026-07-27 08:21:27 -0300

    Update listAdmin.js

--- src/assets/js/pages/listAdmin.js
+++ src/assets/js/pages/listAdmin.js
@@ -12,7 +12,7 @@ async function carregarAdmin() {
 
     try {
 
-        const response = await fetch(`${API}/admin/all?page=0&size=200`, {
+        const response = await fetch(`${API}/admin/all?page=0&size=100`, {
             method: "GET",
             headers: {
                 "Accept": "application/json"
