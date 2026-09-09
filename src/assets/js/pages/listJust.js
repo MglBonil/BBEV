1ba795a2d2d69eda193d3b25db8690e1061b70f7
Kauã Souza <kauasf0504@gmail.com>
2026-09-09 13:30:37 -0300

    Restrict access to admin features based on role
    Hide admin-only elements if user is not admin.

--- src/assets/js/pages/listJust.js
+++ src/assets/js/pages/listJust.js
@@ -130,9 +130,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const sessao = JSON.parse(sessionStorage.getItem("sessaoBBEV"));
     const colunaAcoes = document.getElementById("colunaAcoes");
+    const form = document.getElementById("tableJust");
 
     if (!sessao || sessao.role !== "adm") {
         colunaAcoes.remove();
+        document.querySelectorAll(".somenteAdm").forEach(elemento => {
+            elemento.style.display = "none";
+        });
     }
-
 });
